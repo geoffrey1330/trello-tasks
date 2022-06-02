50530d4049128eba40f4c23af5f83fe496dfe1aa
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-06-02 22:24:52 +0100

    Implemented terraform code style

--- terraform-aws-vpc/main.tf
+++ terraform-aws-vpc/main.tf
@@ -22,7 +22,7 @@ locals {
 }
 
 resource "aws_subnet" "public" {
-  count = length(local.public_cidr)
+  count = 2
 
   vpc_id     = aws_vpc.main.id
   cidr_block = local.public_cidr[count.index]
@@ -33,7 +33,7 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  count = length(local.private_cidr)
+  count = 2
 
   vpc_id     = aws_vpc.main.id
   cidr_block = local.private_cidr[count.index]
@@ -52,12 +52,12 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_eip" "nat" {
-  count = length(local.public_cidr)
+  count = 2
   vpc   = true
 }
 
 resource "aws_nat_gateway" "main" {
-  count = length(local.public_cidr)
+  count = 2
 
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
@@ -81,7 +81,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table" "private" {
-  count = length(local.private_cidr)
+  count = 2
 
   vpc_id = aws_vpc.main.id
 
@@ -96,14 +96,14 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "public" {
-  count          = length(local.public_cidr)
+  count          = 2
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 
 resource "aws_route_table_association" "private" {
-  count          = length(local.private_cidr)
+  count          = 2
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 }
