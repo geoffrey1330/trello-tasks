127a1b9d221e56aed2a9d4ccb27a443e17430bd6
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-06-21 01:28:22 +0100

    added ami data source

--- instance.tf
+++ instance.tf
@@ -16,17 +16,30 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["aws-marketplace"]
+}
 
 resource "aws_instance" "web_public" {
-  count                  = length(local.public_cidr)
-
-  ami                    = "ami-0022f774911c1d690"
+ 
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  key_name               = "${var.env_code}-keypair"
+  key_name               = "main"
 
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  subnet_id              = aws_subnet.public[count.index].id
+  subnet_id              = aws_subnet.public.id
 
   tags = {
     Name = "${var.env_code}-web"
@@ -34,17 +47,15 @@ resource "aws_instance" "web_public" {
 }
 
 
-
 resource "aws_instance" "web_private" {
-  count                  = length(local.public_cidr)
   
-  ami                    = "ami-0022f774911c1d690"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  key_name               = "${var.env_code}-keypair"
+  key_name               = "main"
 
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  subnet_id              = aws_subnet.private[count.index].id
+  subnet_id              = aws_subnet.private.id
 
   tags = {
     Name = "${var.env_code}-web"
