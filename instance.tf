f1539c96e8e9509db0cd34132a1a2b3b6b93d9ed
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-06-22 00:33:19 +0100

    fixed security group cidr

--- instance.tf
+++ instance.tf
@@ -6,13 +6,13 @@ resource "aws_security_group" "web-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24", "10.0.1.0/24"]
   }
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24", "10.0.1.0/24"]
   }
   egress {
     from_port   = 0
