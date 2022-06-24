d8fea197d24a2ce1565aa7ac6932d59f88a3c695
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-06-24 08:06:16 +0100

    added vpc cidr to security group

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "web-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["105.112.102.10"]
+    cidr_blocks = ["105.112.102.10/32","10.0.0.0/16"] 
   }
 
   egress {
