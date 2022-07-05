35f36f118b99223c9090b0142d2ed914054156d9
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-07-05 22:39:33 +0100

    formatted backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "israel-terraform"
-    key    = "trello/state/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "israel-terraform"
+    key            = "trello/state/terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "israel-dynamo-terraform"
   }
 }
-
-
