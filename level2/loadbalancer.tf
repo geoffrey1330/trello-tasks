b7a79cd9d3628cada1be42129d80ef5f7508793e
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-07-22 01:29:15 +0100

    added loadbalancer

--- level2/loadbalancer.tf
+++ level2/loadbalancer.tf
@@ -1,19 +1,12 @@
 resource "aws_security_group" "lb-sg" {
   name   = "${var.env_code}-lb-sg"
-  vpc_id = data.terraform_remote_state.networking.outputs.vpc_id #aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["105.112.100.127/32", "10.0.0.0/16"]
-  }
+  vpc_id = data.terraform_remote_state.networking.outputs.vpc_id 
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["105.112.100.127/32", "10.0.0.0/16"]
+    cidr_blocks = ["105.112.100.127/32"]
   }
 
   egress {
@@ -24,23 +17,12 @@ resource "aws_security_group" "lb-sg" {
   }
 }
 
-
 resource "aws_lb" "app-lb" {
   name               = "${var.env_code}-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb-sg.id]
-  subnets            = [data.terraform_remote_state.networking.outputs.public-subnet_id,data.terraform_remote_state.networking.outputs.private-subnet_id]
-
-  enable_deletion_protection = true
-
-#   access_logs {
-#     bucket  = "israel-terraform"
-#     prefix  = "app-lb"
-#     enabled = true
-#   }
-
+  
   tags = {
-    Environment = "production"
+     Name = "${var.env_code}-lb"
   }
 }
