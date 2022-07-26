4d50c0924842c1f381887e67a97785d023245a47
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-07-27 00:00:10 +0100

    fixed codebase

--- level1/output.tf
+++ level1/output.tf
@@ -10,14 +10,7 @@ output "public-subnet_id" {
   value = aws_subnet.public.*.id
 }
 
-output "public-subnet_id2" {
-  value = aws_subnet.public[1].id
-}
-
 output "private-subnet_id" {
   value = aws_subnet.private.*.id
 }
 
-output "private-subnet_id2" {
-  value = aws_subnet.private[1].id
-}
