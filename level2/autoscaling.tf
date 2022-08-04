96df8c2609cce483fd102c3bf7526cb5510717be
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-08-04 17:24:52 +0100

    fixed autoscaling group

--- level2/autoscaling.tf
+++ level2/autoscaling.tf
@@ -2,14 +2,13 @@ resource "aws_autoscaling_group" "app-asg" {
   launch_configuration = aws_launch_configuration.web_config.id
   vpc_zone_identifier  = data.terraform_remote_state.networking.outputs.public-subnet_id
 
-  load_balancers    = ["${aws_elb.app-lb.name}"]
-  health_check_type = "ELB"
+  load_balancers    = [aws_elb.app-lb.name]
 
   max_size = 5
   min_size = 2
 
   tag {
-    key                 = "name"
+    key                 = "Name"
     value               = "${var.env_code}-asg"
     propagate_at_launch = true
   }
