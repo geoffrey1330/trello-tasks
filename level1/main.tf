bb37d48a624e2276447020e79f9a06a5c7bf1765
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-08-12 00:38:11 +0100

    fixed variable issue

--- level1/main.tf
+++ level1/main.tf
@@ -14,4 +14,7 @@ provider "aws" {
 module "vpc" {
   source   = "../modules/vpc"
   env_code = var.env_code
+  vpc = var.vpc
+  public-subnet = var.public-subnet
+  private-subnet = var.private-subnet
 }
--- modules/vpc/vars.tf
+++ modules/vpc/vars.tf
@@ -4,13 +4,8 @@ variable "env_code" {
 
 variable "vpc" {
   type = string
-  default = "10.0.0.0/16"
 }
 
-variable "public-subnet" {
-  default = ["10.0.0.0/24", "10.0.1.0/24"]
-}
+variable "public-subnet" {}
 
-variable "private-subnet" {
-  default = ["10.0.2.0/24", "10.0.3.0/24"]
-}
+variable "private-subnet" {}
