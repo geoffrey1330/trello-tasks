2dcfebf869de0a758ef0dededd0220f52ab4c33a
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-08-29 17:07:38 +0100

    modified secret manager

--- modules/loadbalancer/secretmanager.tf
+++ modules/loadbalancer/secretmanager.tf
@@ -1,3 +1,11 @@
-data "aws_secretsmanager_secret" "secret_manager" {
-  name = "MyMac/SSH"
+data "aws_secretsmanager_secret" "rds_password" {
+  name = "rds/password"
+}
+
+data "aws_secretsmanager_secret_version" "rds_password" {
+  secret_id     = data.aws_secretsmanager_secret.rds_password.id
+}
+
+locals {
+  rds_password = jsondecode(data.aws_secretsmanager_secret_version.rds_password.secret_string)["password"]
 }
