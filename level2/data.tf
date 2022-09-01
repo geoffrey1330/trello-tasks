09b651bbce62266cfd2eabcd19a78e690c1f395e
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-09-01 14:26:19 +0100

    added rds

--- level2/data.tf
+++ level2/data.tf
@@ -8,3 +8,15 @@ data "terraform_remote_state" "networking" {
     dynamodb_table = "israel-dynamo-terraform"
   }
 }
+
+data "aws_secretsmanager_secret" "rds_password" {
+  name = "rds/password"
+}
+
+data "aws_secretsmanager_secret_version" "rds_password" {
+  secret_id = data.aws_secretsmanager_secret.rds_password.id
+}
+
+locals {
+  rds_password = jsondecode(data.aws_secretsmanager_secret_version.rds_password.secret_string)["password"]
+}
